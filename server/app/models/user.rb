354c342e27038904439624d210df1cdaaafe908f
Erik Peterson <erikeh1@gmail.com>
2018-12-20 20:42:03 -0500

    basic login and create user routes finished

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -2,4 +2,21 @@ class User < ApplicationRecord
   has_secure_password
   has_many :users_events
   has_many :events, through: :users_events
+
+  validates :username, presence: true,  uniqueness: true 
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 7 }, confirmation: true
+
+  def self.authenticate_with_credentials(email, password)
+      user = User.find_by_email(email.strip) 
+      puts "user"
+      puts user
+      puts "&&&&&&"
+      if user && user.authenticate(password.strip)
+        user
+      else
+        nil
+      end
+  end 
+
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     # , only: [:index, :show, :create, :delete]
   end
 
-  get '/users/:user_id/events' => 'user_events#events'
+
 
 
   # resources :user_eve
@@ -22,15 +22,18 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
   end
 
+  get '/users/:user_id/events' => 'user_events#events'
+
+    # these routes are for showing users a login form, logging them in, and logging them out.
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+       # These routes will be for signup. The first renders a form in the browse, the second will 
+     # receive the form and create a user in our database using the data given to us by the user.
+     get '/signup' => 'users#new'
+     post '/users' => 'users#create'
 
 
 end
 
-  #  # these routes are for showing users a login form, logging them in, and logging them out.
-  #  get '/login' => 'sessions#new'
-  #  post '/login' => 'sessions#create'
-  #  get '/logout' => 'sessions#destroy'
-  #     # These routes will be for signup. The first renders a form in the browse, the second will 
-  #   # receive the form and create a user in our database using the data given to us by the user.
-  #   get '/signup' => 'users#new'
-  #   post '/users' => 'users#create'
+ 
