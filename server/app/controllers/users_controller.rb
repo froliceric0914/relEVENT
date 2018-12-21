4b163f3ef74d135ddf1eb753014fc22dc9eda4a8
Erik Peterson <erikeh1@gmail.com>
2018-12-21 12:49:46 -0500

    modified users_controller

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -6,17 +6,32 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(
+    @user = User.new(
       username: params[:username],
       email: params[:email],
       password: params[:password]
     )
 
-    if user.save
-      render json: user
-      # session[:user_id] = user.id
+    if @user.save
+      render :json =>
+      {
+        :status => 'ok',
+        :message => 'User created!',
+        :object => @user
+      }.to_json
     else
-      head :unprocessable_entity
+      render :json =>
+      {
+        :status => 'error',
+        :message => @user.errors.full_messages.to_sentence,
+        :object => @user
+      }.to_json
     end
+  #   if user.save
+  #     render json: user
+  #     # session[:user_id] = user.id
+  #   else
+  #     head :unprocessable_entity
+  #   end
   end
 end
