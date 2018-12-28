9ad9569a18c4d1bab4a2519fe25a82f046f1d6f2
Erik Peterson <erikeh1@gmail.com>
2018-12-27 19:35:21 -0500

    minor chang test

--- client/src/EventList.jsx
+++ client/src/EventList.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Event from "./Event.jsx";
 
-//This is an container to show event listing.
-//TODO: add styling
-
 const EventList = ({ events }) => {
   return (
     <div className="eventList">
