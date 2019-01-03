3af423909d48fa41029e06cf0ce2fba396b1e64b
“az” <shimazaki@aaa.email.ne.jp>
2019-01-03 13:59:03 -0500

    remove test code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -557,18 +557,6 @@ class App extends Component {
           >
             Mylist
           </button>
-          
-
-          {/* <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              myList
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div> */}
 
         </nav>
 
