0ccb4613593cd35e0e54ff871c06c3cec23d6bc4
Erik Peterson <erikeh1@gmail.com>
2019-01-06 17:00:20 -0500

    rough event card formatted

--- client/src/Event.jsx
+++ client/src/Event.jsx
@@ -7,17 +7,15 @@ import { Button, Icon } from "react-materialize";
 //TODO: add onclick to chat button. show chat component
 //TODO: add styling
 const _clickHandler = e => {
-  // console.log("thiddddd", e.target.parentElement.parentElement.parentElement);
-
-  // $(e.target.parentElement.parentElement.parentElement).toggleClass("test");
-  $(e.target.parentElement.parentElement.parentElement).toggleClass("test");
-
-  // ***** come back to dry this
-  // $(this)
-  //   .parentsUntil($(".flip-card-inner"))
-  //   .toggleClass("test");
-
+  $(e.target.parentElement.parentElement.parentElement).toggleClass(
+    "flip-action"
+  );
   console.log("clicked");
+};
+
+const _clickHandler2 = e => {
+  $(e.target.parentElement.parentElement).toggleClass("flip-action");
+  console.log("click", e.target.parentElement.parentElement.parentElement);
 };
 
 const Event = ({
@@ -99,6 +97,10 @@ const Event = ({
     }
   }
 
+  let date = event.start.local.toString();
+  let longDate = new Date(date);
+  let properDate = longDate.toString().substring(0, 10);
+
   // let img_style;
 
   // if (event.logo.url != null) {
@@ -107,118 +109,126 @@ const Event = ({
   // }
 
   return (
-    // <div className="event-card col-6">
-    <div
-      className="card m-5 flip-card"
-      style={{ width: "40rem", height: "32rem" }}
-    >
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <div
-            className="card-img-top"
-            className="event-card-body pb-10"
-            style={{ overflow: "hidden" }}
-            alt="Card image cap"
-            onClick={_clickHandler}
-          >
-            {img}
-          </div>
-          <div className="card-body">
-            <div className="title-date-location">
-              <h2
-                className="card-title text-left"
-                style={{
-                  fontWeight: "600",
-                  height: "4.8rem",
-                  overflow: "hidden"
-                }}
-              >
-                {event.name.text.substring(45)
-                  ? event.name.text.substring(0, 45) + " ..."
-                  : event.name.text}
-              </h2>
-              <h3 className="card-text text-left">{event.start.local} </h3>
-              <h3 className="card-text text-left">
-                {event.venue.address.address_1
-                  ? event.venue.address.address_1.substring(0, 25)
-                  : ""}
-              </h3>
-            </div>
-            {/* <p className="card-text" /> */}
-            <a
-              href="#"
-              className="btn btn-primary detailsButton"
+    <div className="event-card col-4">
+      <div
+        className="card m-5 flip-card"
+        style={{ width: "40rem", height: "32rem" }}
+      >
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <div
+              className="card-img-top"
+              className="event-card-body pb-10"
+              style={{ overflow: "hidden" }}
+              alt="Card image cap"
               onClick={_clickHandler}
             >
-              Details
-            </a>
-            <div className="icons">
-              <i
-                data-on={likeIcon}
-                data-id={event.id}
-                data-name="like"
-                data-event-name={event.name.text}
-                data-img-url={img_url}
-                className={likeclassName}
-                onClick={handleIconClick}
-              />
-              <span className="like-counter">{likeCount}</span>
-              &nbsp;
-              <i
-                data-on={bookmarkIcon}
-                data-id={event.id}
-                data-name="bookmark"
-                data-event-name={event.name.text}
-                data-img-url={img_url}
-                className={bookmarkclassName}
-                onClick={handleIconClick}
-              />
-              <i className="fas fa-bookmark icon whiteBookmark " />
+              {img}
             </div>
-            <div className="iconSideError" />
-            <div className="iconSideMessage" />
-            <button
-              className="chatButton btn btn-danger btn-block mt-4"
-              name={event.id}
-              data-event-name={event.name.text}
-              data-img-url={img_url}
-              onClick={openChat}
-              target="_blank"
+            <div className="card-body">
+              <div className="title-date-location">
+                <h2
+                  className="card-title text-left"
+                  style={{
+                    fontWeight: "600",
+                    height: "4.8rem",
+                    overflow: "hidden"
+                  }}
+                >
+                  {event.name.text.substring(45)
+                    ? event.name.text.substring(0, 45) + " ..."
+                    : event.name.text}
+                </h2>
+                <h3 className="card-text text-left">{properDate}</h3>
+                <h3 className="card-text text-left">
+                  {event.venue.address.address_1
+                    ? event.venue.address.address_1.substring(0, 25)
+                    : ""}
+                </h3>
+              </div>
+
+              <a
+                className="btn btn-primary detailsButton"
+                onClick={_clickHandler}
+              >
+                Details
+              </a>
+              <div className="icons">
+                <i
+                  data-on={likeIcon}
+                  data-id={event.id}
+                  data-name="like"
+                  data-event-name={event.name.text}
+                  data-img-url={img_url}
+                  className={likeclassName}
+                  onClick={handleIconClick}
+                />
+                <span className="like-counter">{likeCount}</span>
+                &nbsp;
+                <i
+                  data-on={bookmarkIcon}
+                  data-id={event.id}
+                  data-name="bookmark"
+                  data-event-name={event.name.text}
+                  data-img-url={img_url}
+                  className={bookmarkclassName}
+                  onClick={handleIconClick}
+                />
+                <i className="fas fa-bookmark icon whiteBookmark " />
+              </div>
+              <div className="iconSideError" />
+              <div className="iconSideMessage" />
+              <button
+                className="chatButton btn btn-danger btn-block mt-4"
+                name={event.id}
+                data-event-name={event.name.text}
+                data-img-url={img_url}
+                onClick={openChat}
+                target="_blank"
+              >
+                Chat
+              </button>
+            </div>
+          </div>
+          <div
+            className="flip-card-back"
+            // style={{
+            //   background: `url(${img_url})`,
+            //   backgroundSize: "cover",
+            //   backgroundRepeat: "no-repeat",
+            //   backgroundColor: "rgba(245, 245, 245, 0.6)",
+            //   backgroundBlendMode: "screen"
+            // }}
+          >
+            <div
+              className="card-img-top"
+              className="event-card-body"
+              style={{ overflow: "hidden", zIndex: "2", height: "10rem" }}
+              onClick={_clickHandler}
+              alt="Card image cap"
             >
-              Chat
+              {img}
+            </div>
+            <p className="event-description">
+              {event.description.text.substring(300)
+                ? event.description.text.substring(0, 300) + "..."
+                : event.description.text}
+            </p>
+            <a
+              href={event.url}
+              target="_blank"
+              className="btn btn-primary btn-block mt-4 more-button"
+            >
+              {" "}
+              More
+            </a>
+            <button
+              className="btn btn-primary backButton"
+              onClick={_clickHandler2}
+            >
+              Back
             </button>
           </div>
-        </div>
-        <div className="flip-card-back">
-          <div
-            className="card-img-top"
-            className="event-card-body"
-            style={{ overflow: "hidden" }}
-            onClick={_clickHandler}
-            alt="Card image cap"
-          >
-            {img}
-          </div>
-          <p>
-            {event.description.text.substring(300)
-              ? event.description.text.substring(0, 300) + "..."
-              : event.description.text}
-          </p>
-          <a
-            href={event.url}
-            target="_blank"
-            className="btn btn-primary btn-block mt-4 more-button"
-          >
-            {" "}
-            More
-          </a>
-          <a
-            href="#"
-            className="btn btn-primary backButton"
-            onClick={_clickHandler}
-          >
-            Back
-          </a>
         </div>
       </div>
     </div>
