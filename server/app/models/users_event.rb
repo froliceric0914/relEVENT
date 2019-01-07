8171dd51806eba9f70072acaaf4c83f8c016ab1d
Erik Peterson <erikeh1@gmail.com>
2019-01-07 17:46:45 -0500

    attempting to activate the email authentication

--- server/app/models/users_event.rb
+++ server/app/models/users_event.rb
@@ -2,25 +2,6 @@ class UsersEvent < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  # validates_uniqueness_of :user_id, :scope => :event_id
+  # CHANGED
+  validates_uniqueness_of :user_id, :scope => :event_id
 end
-
-
-
-# <form onSubmit={this.onSubmit}>
-#   <input value={this.state.name} onChange={(e) => this.setState(name: e.target.value)}
-# </form>
-
-# onSubmit = () => {
-#   fetch('localhost:8080/events', {
-#     method: "POST",
-#     body: JSON.strinifgy{
-#       name: this.state.name,
-#       .....
-#     })
-#   })
-#   .then(res => res.json())
-#   .then(event => {
-#     // TODO: Render something else
-#   })
-# }
