bb6bbdad78d97dfc23b2a94d7ced121af0ba14ed
“az” <shimazaki@aaa.email.ne.jp>
2019-01-08 00:53:37 -0500

    add image posting

--- client/src/Message.jsx
+++ client/src/Message.jsx
@@ -9,6 +9,35 @@ class Message extends Component {
 
   render() {
 
+    let newContent = [];
+    let tmpContent = this.props.message.content;
+
+    // make an array by white space separated words
+    let contentBlocks= tmpContent.match(/([^\s]+)/g);
+    
+      contentBlocks.forEach((block, index)=>{       
+
+          // if a block was an image url, set html img
+          if(block.match(/((https|http):\/\/[^\s]*\.(jpg|png|gif))/g)){
+            newContent.push(<div key={index} style={{margin: '0 -5px', overflow: 'hidden'}}><img key={index} src={block}/></div>); 
+              
+          // if the block was the last one and not image url, set html span  
+          }else if(index === contentBlocks.length-1){
+            newContent.push(<span key={index} >{block}</span>);
+
+          // if the block was not image url, add white space, and set html span  
+          }else{
+            newContent.push(<span key={index} >{block}&nbsp;</span>);
+          }
+      });
+
+    let message =   
+          <div className='message'>
+            {/* <span className='message-username'  style={{color: "blue"}}>{this.props.message.user.username}</span> */}
+            <span className='message-content' ><div className="content-wrapper">{newContent}</div></span>
+          </div>;
+
+
     let date = new Date(this.props.message.created_at);
     let stringDate = date.toString().substring(0, 25);
 
@@ -18,13 +47,14 @@ class Message extends Component {
       // we will adjust about user colour later
 
       <div className="userMessageContainer" style={{color: isCurrentUser ? "blue" : "#000"}}>
+        
         <div className="messageInfoContainer">
           <img src="./images/user-image-2.png" className="userImage"></img>
           {this.props.message.user.username}
-          {isCurrentUser ? "  (You):   " : ""}
+          {isCurrentUser ? "  (You)" : ""}
         </div>
            &nbsp;
-          <p className="messageContent">{this.props.message.content}</p>
+          <p className="messageContent">{message}</p>
           <p className="messageTime">{stringDate}</p>
       </div>
     );
