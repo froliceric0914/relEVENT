1a7400bbaeba6a6a480d0662d2c4a2232554a5fb
Erik Peterson <erikeh1@gmail.com>
2019-01-15 17:56:46 -0500

    finished cleaning up the server side -lots of instructional comments still left

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,23 +2,9 @@ Rails.application.routes.draw do
 
   mount ActionCable.server => '/cable'
 
-  # changed this to add show
   resources :users do
     resources :user_events
-    # , only: [:index, :show, :create, :delete]
   end
-
-
-
-
-  # resources :user_eve
-
-  # resources :events
-  # get '/events' => 'events#index'
-  # get '/events/:id' => 'events#show'
-  # post '/events' => 'events#create'
-  # put '/events/:id' => 'events#update'
-  # delete '/events/:id' => 'events#delete'
 
   resources :events, only: [:show, :index] do
     resources :messages, only: [:index, :create]
@@ -32,9 +18,8 @@ Rails.application.routes.draw do
     post '/login' => 'sessions#create'
     get '/logout' => 'sessions#destroy'
        # These routes will be for signup. The first renders a form in the browse, the second will
-     # receive the form and create a user in our database using the data given to us by the user.
+       # receive the form and create a user in our database using the data given to us by the user.
      get '/signup' => 'users#new'
      post '/users' => 'users#create'
-
 
 end
